1a8e555524f035d4769ffb26c81bb519588501e9
hong2323 <42904811+hong2323@users.noreply.github.com>
2019-10-04 09:35:53 +0700

    Update js.js

--- weather/js.js
+++ weather/js.js
@@ -6,7 +6,7 @@ Vue.component('weather-day', {
   computed: {
     weekday() {
       let d = new Date(this.day.Date);
-      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      let days = ["Chủ nhật", "Thứ Hai", "Thứ Ba", "Thứ Tư", "Thứ Năm", "Thứ Sáu", "Thứ Bảy"];
       return days[d.getDay()];
     },
 
